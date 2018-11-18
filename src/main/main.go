aa2d4330d20f344b93b7eff637cdeb9860771ca3
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2018-11-18 01:15:58 -0500

    Starts a server

--- src/main/main.go
+++ src/main/main.go
@@ -2,11 +2,17 @@ package main
 
 
 import (
-    "test"
+    "log"
+    "net/http"
     "fmt"
 )
 
+func hanlder(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, r.URL.Path)
+}
+
 func main() {
-    output := test.Test("please work")
-    fmt.Print(output)
+    http.HandleFunc("/", hanlder)
+    fmt.Println("Listening on port 8000...")
+    log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- src/test/test.go
+++ src/test/test.go
@@ -1,5 +1,9 @@
 package test
 
+import (
+    "fmt"
+)
+
 func Test(text string) string {
     return text
 }
