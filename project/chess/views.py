79ed37dc30abfcec2ecf7bb147659b6df2b25ff9
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2018-12-27 01:08:44 -0500

    basic project setup and working

--- project/chess/views.py
+++ project/chess/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, World")
+    return render(request, "chess.html")
+
+def test(request):
+    response = {"response_data": "it worked"}
+    return JsonResponse(response)
--- project/project/settings.py
+++ project/project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -55,7 +54,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'chess/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS  = [
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chess/staticfiles'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
