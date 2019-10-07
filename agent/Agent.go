48606bfc5dc6bec912d5bd9f308a2687365a5f94
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2019-10-06 18:18:33 -0700

    refined mutating agent

--- agent/Agent.go
+++ agent/Agent.go
@@ -8,13 +8,12 @@ import (
 
 type Agent struct {
     matrix [][]int
-    activations map[int]func(float64)float64
     inputs int
     outputs int
 }
 
 func Initialize(inputs, outputs int) *Agent {
-    max := 1000
+    max := 30
 
     agent := new(Agent)
     agent.matrix = make([][]int, max)
@@ -26,22 +25,13 @@ func Initialize(inputs, outputs int) *Agent {
     agent.outputs = outputs
 
     //seed random
-    rand.Seed(time.Now().UTC().UnixNano())
 
     for i := 0; i < outputs; i++ {
-        from := rand.Intn(inputs)
+        from := randint(inputs)
         agent.matrix[from][max - i - 1] = 1
     }
 
     return agent
-}
-
-func add_node(agent *Agent) *Agent {
-    //Get Source Node
-    
-    //Point to somewhere random w/ constraints
-    //Make random source point to previously pointed
-    //Make initial source point to random node
 }
 
 func main() {
@@ -49,4 +39,5 @@ func main() {
     for i := range agent.matrix {
         fmt.Println(agent.matrix[i])
     }
+    addNode(agent)
 }
